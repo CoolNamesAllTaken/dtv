f5012a78cf9dad2afbf0142a29ac8a32c9946724
John McNelly <jkailimcnelly@gmail.com>
2022-10-22 23:45:36 -0700

    Givin it a go

--- dtv-server/dtv_server/dtv_site/branch_services/models.py
+++ dtv-server/dtv_server/dtv_site/branch_services/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+def treat_license_photo_path(instance, filename):
+    # image will be uploaded to MEDIA_ROOT/treat_license_photos/{id}/John_M_2022101902.jpg
+    return 'treat_license_photos/{}_{}_{}/{}-{}_{}-{}'.format(
+        date.today().year,
+        date.today().month,
+        date.today().day
+        instance.id,
+        instance.first_name,
+        instance.last_initial,
+        instance.filename) # TODO: figure out if instance.id will work here
+
+class TreatLicenseCard(models.Model):
+    first_name = models.CharField(max_length=32) # TODO: set these
+    last_initial = models.CharField(max_length = 1)
+    photo = models.FileField(upload_to=treat_license_photo_path)
+    expiration_date = models.DateField(default=date.today().replace(year=date.today().year + 1)) # Treat license expires in one year
+    treat_class = models.CharField(max_length=5)
+    # treat_weight_oz = models.
+    profession = models.CharField(max_length=32)
+    favorite_color = models.CharField(max_length=4)
+    favorite_animal = models.CharField(max_length=32)
+    
--- dtv-server/dtv_server/dtv_site/branch_services/views.py
+++ dtv-server/dtv_server/dtv_site/branch_services/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the branch services app!")
+    return render(request, "branch_services/index.html")
 
 def id_request(request):
+    pass
